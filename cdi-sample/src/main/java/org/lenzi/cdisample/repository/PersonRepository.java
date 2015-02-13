bae15dc6face15a61de02bcb0b2938d919bf2a0a
sal <sal@FSTRF-lenovoSSD.dfci.harvard.edu>
2015-02-13 18:16:17 -0500

    jpa not done

--- cdi-sample/src/main/java/org/lenzi/cdisample/repository/PersonRepository.java
+++ cdi-sample/src/main/java/org/lenzi/cdisample/repository/PersonRepository.java
@@ -1,5 +1,13 @@
 package org.lenzi.cdisample.repository;
 
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
+import org.lenzi.cdisample.model.prs.Person;
+
 /**
  * Repository for person data...
  * 
@@ -15,6 +23,22 @@ public class PersonRepository extends AbstractRepository {
 
 	public PersonRepository() {
 		
+	}
+	
+	public Person getPersonById(int id){
+		
+		CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Person> query	= cb.createQuery(Person.class);
+		Root<Person> root = query.from(Person.class);
+		ParameterExpression<Integer> p = cb.parameter(Integer.class);
+		query.select(root).where(cb.equal(root.get("personId"), p));
+		//query.
+		
+		try{
+			return getEntityManager().createQuery(query).getSingleResult();
+		} catch (NoResultException e){
+			return null;
+		}		
 	}
 
 }
