6595bb676052731f723d18622b71d224f771679d
slenzi <slenzi@gmail.com>
2015-02-16 15:22:35 -0500

    appears to work, but not quite pleased. Need to do some cleanup.

--- cdi-sample/src/main/java/org/lenzi/cdisample/producer/EntityManagerProducer.java
+++ cdi-sample/src/main/java/org/lenzi/cdisample/producer/EntityManagerProducer.java
@@ -1,25 +1,33 @@
 package org.lenzi.cdisample.producer;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 import org.lenzi.cdisample.db.repository.PostgresUnit;
 
-//@ApplicationScoped
-//@SessionScoped
+@ApplicationScoped
 public class EntityManagerProducer {
 
-	@PersistenceContext(unitName="PostgresPersistenceUnit")
-	private EntityManager postgresEntityManager;
+	//@PersistenceContext(unitName="PostgresPersistenceUnit")
+	//private EntityManager postgresEntityManager;
 	
-	//@RequestScoped
-	//@Produces
-	//@Default
-	//@Dependent
+	@Inject
+	private EntityManagerFactory emf;
 	
+	@Produces
+	@RequestScoped
+	@PostgresUnit
+	private EntityManager getEntityManager(){
+		return emf.createEntityManager();
+	}
+	
+	/*
 	@Produces
 	@RequestScoped
 	@PostgresUnit
@@ -29,11 +37,14 @@ public class EntityManagerProducer {
 		}
 		return postgresEntityManager;
 	}
+	*/
+	
 	
     protected void closeEntityManager(@Disposes @PostgresUnit EntityManager entityManager){
         if (entityManager.isOpen()){
             entityManager.close();
         }
     }
+    
 
 }
