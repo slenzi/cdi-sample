488f876fd5341fd714dad99a61c4a2a0479944f0
slenzi <slenzi@gmail.com>
2015-02-21 09:31:00 -0500

    comment

--- cdi-sample/src/main/java/org/lenzi/cdisample/producer/LoggerProducer.java
+++ cdi-sample/src/main/java/org/lenzi/cdisample/producer/LoggerProducer.java
@@ -10,6 +10,13 @@ import org.slf4j.LoggerFactory;
 @Dependent
 public class LoggerProducer {
 
+	/**
+	 * The logger will be created with the class name of the class that it was
+	 * injected into, aka the injection point.
+	 * 
+	 * @param injectionPoint
+	 * @return
+	 */
 	@Produces
 	public Logger getLogger(InjectionPoint injectionPoint){
 		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
